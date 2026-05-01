a626609beb3f65e37a864916e915ec67a540690d
sepryandayon810-debug <sepryandayon810@gmail.com>
2026-05-01 17:15:31 +0700

    Create firebase-config.js

---
+++ js/core/firebase-config.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSC05MTnaiiSftj1TA-LVCH4ymHBAbkoU",
+  authDomain: "hifzicell-v2.firebaseapp.com",
+  databaseURL: "https://hifzicell-v2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "hifzicell-v2",
+  storageBucket: "hifzicell-v2.firebasestorage.app",
+  messagingSenderId: "766095621773",
+  appId: "1:766095621773:web:da40a79eab0f573683acc4",
+  measurementId: "G-N9B9PKFCHT"
+};
+
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.database();
+window.auth = firebase.auth();
+
+// Helper
+window.getUid = () => auth.currentUser ? auth.currentUser.uid : null;
+window.genId = (prefix) => prefix + "-" + Date.now().toString(36).toUpperCase();
+window.nowIso = () => new Date().toISOString();
+window.todayKey = () => new Date().toISOString().split("T")[0];
